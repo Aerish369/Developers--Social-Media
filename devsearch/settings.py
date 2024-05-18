68a495ba098b25706538883a1a04ff683434b4d7
Aerish <aryalaerish369@gmail.com>
2024-05-18 16:26:17 +0545

    configured basic apis

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -22,7 +22,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
 
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -114,12 +113,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'fakeemail@gmail.com'
-EMAIL_HOST_PASSWORD = 'fakepassword123'
+EMAIL_HOST_USER = 'dummyemail@gmail.com'
+EMAIL_HOST_PASSWORD = 'dummypassword'
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/'rmuq namn kkhv tuve'
+# https://docs.djangoproject.com/en/4.2/howto/static-files/'rmuq namn kkhv tuve'/django-admin startproject devsearch
 
 STATIC_URL = 'static/'
 
