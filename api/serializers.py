43c8e34b570006dbc41de5d3090a521d1ac94c84
Aerish <aryalaerish369@gmail.com>
2024-05-19 21:50:45 +0545

    serializers relationship configed

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,35 @@
 from rest_framework import serializers
-from projects.models import Project
+from projects.models import Project, Tag, Review
+from users.models import Profile
 
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'value', 'body', 'owner', 'project']
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['id', 'name', 'email', 'username', 'short_intro']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+ 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    owner = ProfileSerializer()
+    tags = TagSerializer(many=True)
+    reviews = serializers.SerializerMethodField()
+
     class Meta:
         model = Project
         fields = '__all__'
+
+    def get_reviews(self, obj):
+        reviews = obj.review_set.all()
+        serializer = ReviewSerializer(reviews, many=True)   
+        return serializer.data
