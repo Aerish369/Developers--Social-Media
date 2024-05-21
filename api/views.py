8fd20433bb50d166071cf10dc7f1a4cedfd88c49
Aerish <aryalaerish369@gmail.com>
2024-05-21 22:14:33 +0545

    setup simplejwt

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from projects.models import Project
@@ -21,10 +22,11 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getProjects(request):
+    print('User:', request.user)
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
-
     return Response(serializer.data)
 
 
